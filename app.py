621c13c628b5a4ef45f4bd0fa84410f1cfa521d5
Betty Guo <bettyg3113@gmail.com>
2024-01-04 16:18:52 -0500

    pushing app

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+__all__ = ['is_cat', 'learn', 'classify_image', 'categories', 'image', 'label', 'examples', 'intf']
+
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper() 
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+learn = load_learner('model.pkl')
+
+labels = learn.dls.vocab
+
+def classify_image(img):
+    pred,pred_idx,probs = learn.predict(img)
+    return dict(zip(labels, map(float, probs)))
+
+image = gr.inputs.Image(shape=(224, 224))
+label = gr.outputs.Label()
+examples = ['siamese.jpg', 'beagle.jpg', 'bagel.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
